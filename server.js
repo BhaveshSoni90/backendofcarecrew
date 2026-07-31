9e5306533f0222fcd50e207cd0d978ea5199f22f
BhaveshSoni90 <160270545+BhaveshSoni90@users.noreply.github.com>
2026-07-31 10:06:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://carecrew:bhama90@carecrew.9r659.mongodb.net/?retryWrites=true&w=majority&appName=carecrew' || process.env.MONGODB_URI, {
+//t('mongodb+srv://carecrew:bhama90@carecrew.9r659.mongodb.net/?retryWrites=true&w=majority&appName=carecrew' 
+mongoose.connect('mongodb+srv://bhaveshbhama827:bhama90@@carecrewapi.vp0dj3o.mongodb.net/?appName=carecrewapi' || process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
